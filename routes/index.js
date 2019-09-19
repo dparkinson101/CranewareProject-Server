4a1b429a86a3be784d771f36e7869c11dd666df8
Owen <owenkelbie98@gmail.com>
2019-09-19 14:49:13 +0100

    Added more queries
    Add two simple filters and query to get all the providers information

--- routes/index.js
+++ routes/index.js
@@ -18,9 +18,9 @@ router.get('/', function(req, res, next) {
 /*GET request - return top 10 records, currently for testing purposes FOR AZURE SERVER CONNECTION*/
 router.get('/azure', function(req, res, next) {
 
-    var parameters = req.query.code;
+    var code = req.query.code;
     var req = new sql.Request(connection);
-    req.query(('SELECT TOP 10 * FROM [DataSet] where substring(dRGDefinition, 1, 3)=' + parameters), function(err, results) {
+    req.query(('SELECT TOP 10 * FROM [DataSet] where substring(dRGDefinition, 1, 3)=' + code), function(err, results) {
 
         if (err) {
             res.status(500).json({ "status_code": 500, "status_message": "internal server error" + err });
@@ -79,9 +79,9 @@ router.delete('/user', function(req, res) {
 /*FOR SILVA SQL DATABASE CONNECTION */
 router.get('/silva', function(req, res, next) {
 
-    var parameters = req.query.code;
+    var code = req.query.code;
 
-    connection.query(('SELECT * FROM overnighttest2 WHERE substring(dRGDefinition, 1, 3)=' + parameters + ' LIMIT 10'), function(err, results) {
+    connection.query(('SELECT * FROM alldata WHERE substring(dRGDefinition, 1, 3)=' + code + ' LIMIT 10'), function(err, results) {
 
         if (err) {
             console.log(err);
@@ -93,6 +93,82 @@ router.get('/silva', function(req, res, next) {
 
     });
 });
+
+//query to sort the search results by price in ascending order
+router.get('/sortpriceasc', function(req, res, next) {
+
+    var code = req.query.code;
+
+    connection.query(('SELECT * FROM alldata WHERE substring(dRGDefinition, 1, 3)=' + code + ' ORDER BY averageTotalPayments ASC LIMIT 10'), function(err, results) {
+
+        if (err) {
+            console.log(err);
+            res.status(500).json({ "status_code": 500, "status_message": "internal server error" + err });
+        } else {
+
+            res.send(results); //returns the results as JSON
+        }
+
+    });
+});
+
+
+//query to filter the results by providerZipCode
+router.get('/filterzipcode', function(req, res, next) {
+
+    var code = req.query.code
+    var zipcode = req.query.zipcode
+
+    connection.query(('SELECT * FROM overnighttest2 WHERE substring(dRGDefinition, 1, 3)=' + code + ' AND providerZipCode='+zipcode+' LIMIT 10'), function(err, results) {
+
+        if (err) {
+            console.log(err);
+            res.status(500).json({ "status_code": 500, "status_message": "internal server error" + err });
+        } else {
+
+            res.send(results); //returns the results as JSON
+        }
+
+    });
+});
+
+router.get('/filterstate', function(req, res, next) {
+
+    var code = req.query.code;
+    var state = req.query.state;
+
+    connection.query(('SELECT * FROM overnighttest2 WHERE substring(dRGDefinition, 1, 3)=' + code + ' AND providerState='+state+' LIMIT 10'), function(err, results) {
+
+        if (err) {
+            console.log(err);
+            res.status(500).json({ "status_code": 500, "status_message": "internal server error" + err });
+        } else {
+
+            res.send(results); //returns the results as JSON
+        }
+
+    });
+});
+
+//query to get the provider info from procedure code
+router.get('/providerinfo', function(req, res, next) {
+
+    var code = req.query.code;
+
+    connection.query(('SELECT providerId, providerName, providerStreetAddress, providerCity, providerState, providerZipCode, averageTotalPayments FROM alldata WHERE substring(dRGDefinition, 1, 3)=' + code + ' LIMIT 10'), function(err, results) {
+
+        if (err) {
+            console.log(err);
+            res.status(500).json({ "status_code": 500, "status_message": "internal server error" + err });
+        } else {
+
+            res.send(results); //returns the results as JSON
+        }
+
+    });
+});
+
+
 
 
 module.exports = router;
