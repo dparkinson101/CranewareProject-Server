fc0da0387a1e56f532683473e3aacde0db9db0ac
Owen <owenkelbie98@gmail.com>
2019-09-19 14:59:01 +0100

    Update index.js
    Fixed error where wrong table was being used

--- routes/index.js
+++ routes/index.js
@@ -119,7 +119,7 @@ router.get('/filterzipcode', function(req, res, next) {
     var code = req.query.code
     var zipcode = req.query.zipcode
 
-    connection.query(('SELECT * FROM overnighttest2 WHERE substring(dRGDefinition, 1, 3)=' + code + ' AND providerZipCode='+zipcode+' LIMIT 10'), function(err, results) {
+    connection.query(('SELECT * FROM alldata WHERE substring(dRGDefinition, 1, 3)=' + code + ' AND providerZipCode='+zipcode+' LIMIT 10'), function(err, results) {
 
         if (err) {
             console.log(err);
@@ -137,7 +137,7 @@ router.get('/filterstate', function(req, res, next) {
     var code = req.query.code;
     var state = req.query.state;
 
-    connection.query(('SELECT * FROM overnighttest2 WHERE substring(dRGDefinition, 1, 3)=' + code + ' AND providerState='+state+' LIMIT 10'), function(err, results) {
+    connection.query(('SELECT * FROM alldata WHERE substring(dRGDefinition, 1, 3)=' + code + ' AND providerState='+state+' LIMIT 10'), function(err, results) {
 
         if (err) {
             console.log(err);
